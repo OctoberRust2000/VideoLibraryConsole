830fe179b55783b311616fcacde5edf48e835dce
Marek Januszkiewicz <suszony.dzik@gmail.com>
2017-11-28 23:11:02 +0100

    Traversing in nested directories and searching only for video format files.

--- VideoLibraryConsole/Program.cs
+++ VideoLibraryConsole/Program.cs
@@ -9,20 +9,85 @@ namespace VideoLibraryConsole
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static int directoryCounter = 0;
+        static int filesCounter = 0;
+        static List<String> fileExtensions = new List<String>();
+        static List<String> fileExtensionsDistinct = new List<String>();
+        static string[] videoFileExtensions = {".webm", ".mkv", ".flv", ".flv", ".vob", ".ogv", ".ogg", ".drc", ".mng", ".avi", ".mov", ".qt", ".wmv", ".yuv", ".rm", ".rmvb", ".asf", ".amv", ".mp4", ".m4p", ".m4v", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".mpg", ".mpeg", ".m2v", ".m4v", ".svi", ".3gp", ".3g2", ".mxf", ".roq", ".nsv", ".flv", ".f4v", ".f4p", ".f4a", ".f4b"};
+        static List<String> videoFiles = new List<String>();
+
+		static void Main(string[] args)
+        {
+            
+            string directoryPath = "X:\\";
+
+            //GetDirectories(directoryPath);
+
+            GetFiles(directoryPath);
+
+            fileExtensionsDistinct = fileExtensions.Distinct().ToList();
+
+            Console.WriteLine("Directories number: " + directoryCounter);
+            Console.WriteLine("Files number: " + filesCounter);
+
+            Console.WriteLine("File extensions:");
+            foreach (var fileExtension in fileExtensionsDistinct)
+            {
+                Console.WriteLine(fileExtension);
+            }
+
+			Console.WriteLine("Video files:");
+			foreach (var videoFile in videoFiles)
+			{
+				Console.WriteLine(videoFile);
+			}
+
+			Console.ReadKey();
+        }
+
+
+        static void GetDirectories(string directoryPath)
+        {
+            string[] directories;
+
+            directories = Directory.GetDirectories(directoryPath);
+            GetFiles(directoryPath);
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                directoryCounter++;
+                //Console.WriteLine(directories[i]);  
+                GetDirectories(directories[i]);
+            }
+        }
+
+
+        static void GetFiles(string directoryPath)
         {
             string[] files;
+            string fileExtension;
 
-            Console.WriteLine("Hello!");
-
-            files = Directory.GetFiles("X:\\");
+            files = Directory.GetFiles(directoryPath);
 
             for (int i = 0; i < files.Length; i++)
             {
-                Console.WriteLine(files[i]);
+                filesCounter++;
+                //Console.WriteLine(files[i]);
+                fileExtension = GetFileExtension(files[i]);
+                fileExtensions.Add(fileExtension);
+				
+				if (videoFileExtensions.Contains(fileExtension))
+				{
+					videoFiles.Add(files[i]);
+				}
             }
+        }
 
-            Console.ReadKey();
+
+        static string GetFileExtension(string filename)
+        {
+            return Path.GetExtension(filename).ToLower();
         }
     }
 }
