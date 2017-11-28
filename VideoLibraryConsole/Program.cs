209e9a3953891ecd8e800cda438068e0ac6a71eb
Marek Januszkiewicz <suszony.dzik@gmail.com>
2017-11-28 21:52:33 +0100

    Initial commit

---
+++ VideoLibraryConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoLibraryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files;
+
+            Console.WriteLine("Hello!");
+
+            files = Directory.GetFiles("X:\\");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
